6bed5bfc84bf0ec73c15d4777a2f8c8b643a4d9e
潘天乡 <pantianxiang@pantianxiangdemacbook-pro.local>
2018-06-02 18:20:55 +0800

    Initial Commit

---
+++ Algorithm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorithm
+//
+//  Created by 潘天乡 on 02/06/2018.
+//  Copyright © 2018 PTX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Algorithm/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Algorithm
+//
+//  Created by 潘天乡 on 02/06/2018.
+//  Copyright © 2018 PTX. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
